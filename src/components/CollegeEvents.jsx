b81b920f81a45a2546dcfa336665c0c48ae95d1a
Estel Zoe <estelzoe433@gmail.com>
2025-06-15 20:50:33 +0000

    done all i was assigned to do, which was not found page,viewcollege page

--- src/components/CollegeEvents.jsx
+++ src/components/CollegeEvents.jsx
@@ -3,10 +3,10 @@ import EventCard from "./EventCard";
 export default function CollegeEvents() {
     return (
         <section className="mt-20 w-[90%] mx-auto">
-            <div>
-                <h1>
-                    <span>College</span>
-                    <span>Events</span>
+            <div className="text-left mb-8">
+                <h1 className="text-2xl md:text-3xl font-bold ">
+                    <span className="text-gray-600 mr-2">College</span>
+                    <span className="text-purple-800">Events</span>
                 </h1>
             </div>
             <div className="grid grid-cols-3 gap-5">
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,31 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { Instagram, Facebook, Linkedin, Twitter, Youtube, } from "lucide-react";
+import Oops from "../assets/404.svg"
+import { Link } from "react-router";
 
 export default function NotFound() {
     return (
         <>
             <Navbar />
-            <section>
-                <h1>Welcome to the 404 page</h1>
+            <section className="min-h-screen flex flex-col items-center justify-center text-center px-4">
+                <img src={Oops} alt="404" className="w-64 md:w-94 mb-6" />
+                <div>
+                    <h1 className="text-3xl md:text-4xl font-bold mt-6 mb-10 text-gray-800">Oops!</h1>
+                    <p className="text-gray-600 mt-2 mb-6">We can't seem to find the page you looking for</p>
+                </div>
+                <Link to="/" ><button className="bg-purple-400 text-white px-4 py-2 hover:bg-purple-700 rounded-full text-sm font-bold ">Go To HomePage</button></Link>
+                <div className="mt-8">
+                    <p className="text-black-100 font-bold mb-2">Follow Us On</p>
+                    <div className="flex justify-center gap-4 text-purple-400 text-2xl">
+                        <Instagram />
+                        <Facebook />
+                        <Linkedin />
+                        <Twitter />
+                        <Youtube />
+                    </div>
+                </div>
+
             </section>
             <Footer />
         </>
