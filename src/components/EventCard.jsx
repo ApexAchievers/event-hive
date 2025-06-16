5a84764bb46421947586d5202a29cb17f69cc479
Estel Zoe <estelzoe433@gmail.com>
2025-06-16 11:08:36 +0000

    styled the cards

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -3,11 +3,17 @@
 
 export default function EventCard() {
     return (
-        <div>
-            <img src={'https://plus.unsplash.com/premium_photo-1661306437817-8ab34be91e0c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZXZlbnRzfGVufDB8fDB8fHww'} alt="Event Card" />
-            <h1>BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow</h1>
-            <h2>Saturdat, March 18, 9.30PM</h2>
-            <h3>ONLINE EVENT - Attend anywhere</h3>
+        <div className="bg-white rounded-xl shadow-md overflow-hidden w-full max-w-sm">
+            <img src={'https://plus.unsplash.com/premium_photo-1661306437817-8ab34be91e0c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZXZlbnRzfGVufDB8fDB8fHww'} alt="Event Card" className="w-full h-48 object-cover" />
+
+            <div className="p-4">
+                <h1 className="text-lg font-semibold text-gray-800 mb-2">BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow</h1>
+                <h2 className="text-sm text-gray-600 mb-1">Saturday, March 18, 9.30PM</h2>
+                <h3 className="text-sm text-purple-700 font-medium">ONLINE EVENT - Attend anywhere</h3>
+                <h1 className="text-lg font-semibold text-gray-800 mb-2">BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow</h1>
+                <h2 className="text-sm text-gray-600 mb-1">Saturday, March 18, 9.30PM</h2>
+                <h3 className="text-sm text-purple-700 font-medium">ONLINE EVENT - Attend anywhere</h3>
+            </div>
         </div>
     );
 }
