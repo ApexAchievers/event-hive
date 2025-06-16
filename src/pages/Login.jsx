fea39ee08b81df480241b01c0cc0da875af68637
Kojo Oppong <princedarf1@.com>
2025-06-16 04:41:16 +0000

    worked on sign up and login pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,60 @@
+
+
 export default function Login() {
     return (
-        <div>
-            <h1>Welcome to the Login Page</h1>
-        </div>
+        <>
+            <div className="flex min-h-screen">
+                {/* Left Section - Form */}
+                <div className="md:w-1/2 bg-gray-200 flex items-center justify-evenly p-8">
+                    <div className="max-w-md w-full">
+                        <h2 className="text-heading font-semibold text-center"> <span className="font-bold">Event</span> <span className="text-[#7848F4] font-bold">Hive</span></h2>
+                        <h3 className="text-big-heading font-bold mt-6 mb-8 text-center tracking-tighter">Sign In to Event Hive</h3>
+
+                        <form className="space-y-6">
+                            <div>
+                                <label className="mb-1  text-body-text">YOUR EMAIL</label>
+                                <input type="email" className="w-full px-4 py-2 border border-gray-300 rounded-[5px] bg-white" placeholder="Enter your mail" />
+                            </div>
+                            <div>
+                                <label className="mb-1 text-body-text">PASSWORD</label>
+                                <input type="password" className="w-full px-4 py-2 border border-gray-300 rounded-[5px] bg-white" placeholder="Enter your password" />
+                                <p className="text-right mt-1">
+                                    <a href="#" className="text-sm text-gray-500 hover:text-gray-800 a">Forgot your password?</a>
+                                </p>
+                            </div>
+                            <button
+                                type="submit"
+                                className="flex justify-evenly text-center  items-center gap-[10px] border-[1px] w-[257px] h-[40px] ml-24 bg-[#7848F4] text-white rounded-[5px] hover:bg-purple-700 transition"
+                            >
+                                Sign In
+                            </button>
+                        </form>
+
+                        <div className="my-4 flex justify-center mr-2 text-center text-gray-500">Or</div>
+
+                        <button className=" border-[1px] border-gray-300 py-2 rounded-[5px] w-[371px] h-[46px] ml-8 flex items-center justify-center gap-2 hover:bg-gray-50 transition">
+                            <img src="https://www.svgrepo.com/show/355037/google.svg" alt="Google" className="w-5 h-5" />
+                            Sign up with Google
+                        </button>
+                    </div>
+                </div>
+
+                {/* Right Section - Image/Message */}
+                <div className="hidden md:flex w-[600px] bg-cover bg-center relative bg-[url(./assets/images/login.png)] h-screen">
+                    <div className="bg-opacity-50 text-white p-10 w-full flex items-center justify-center">
+                        <div className="text-center">
+                            <h2 className="text-heading font-bold mb-2">Hello Friend</h2>
+                            <p className="text-body-text">To keep connected with us, please login with your personal info</p>
+
+                        </div>
+                        <button className="mb-12 absolute border-white py-2 px-6 hover:bg-white hover:text-black transition flex justify-around w-[128px] h-[49px] top-[498px] left-[233.5px] border-[5px] border-none pt-[5px] pr[40px] pb-[15px] pl-[40px] gap-[10px] bg-white/40 text-[#FFFFFF] font-sans font-[16px] rounded-md items-center cursor-pointer">
+                            Sign Up
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+
+        </>
     );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,56 @@
+import googleIcon from '../assets/images/google-icon.png';
+
 export default function Register() {
     return (
-        <div>
-            <h1>Welcome to the Register Page</h1>
-        </div>
+        <>
+
+
+            <div className="flex bg-gray-200 w-full h-[900px] rounded-b-lg">
+                <div className="bg-[url(./assets/images/register.png)] bg-cover w-[680px] h-[850px] rounded-bl-md rounded-tl-md">
+                    <div className="flex justify-center items-center text-white">
+                        <h2 className="absolute font-sans font-body-text text-[40px] text-center text-[#FFFFFF] leading-[100%] items-center w-[280px] h-[49px] top-[350px] left-[160px]">Welcome back</h2>
+                        <p className=" absolute w-[412px] h-[19px] top-[439px] left-[94px] text-body-text font-sans font-normal leading-px text-center text-[#FFFFFF] tracking-tight">To keep connected with us provide us with your information </p>
+                        <button className=" absolute flex w-[128px] h-[49px] top-[498px] left-[233.5px] border-[5px] border-none pt-[5px] pr[40px] pb-[15px] pl-[40px] gap-[10px] bg-white/40 text-[#FFFFFF] font-sans font-[16px] rounded-md items-center cursor-pointer">Signin</button>
+                    </div>
+
+                </div>
+
+                <div className="flex flex-row justify-around items-center w-[840px] h-[900px] top-[11px] left-[493px] bg-gray-200">
+                    <div className="w-[578px] h-[669px] top-[96px] left-[731px] border-0">
+                        <div className="max-w-md w-full space-y-6">
+                            <h3 className="flex flex-row justify-center">Event <span className="text-[#7848F4]">Hive</span></h3>
+                            <h3 className="text-big-heading font-sans text-[#000000] text-center font-bold text-4xl tracking-tight h-[44px] mb-4">Sign Up to Event Hive</h3>
+                        </div>
+
+                        <form className="relative space-y-4">
+                            <label htmlFor="" className="font-sans font-normal text-base items-center leading-[100%] w-[578px] h-[19px] tracking-tight">YOUR NAME</label>
+                            <input type="text" placeholder="Enter your name" className="flex w-[578px] h-[46px] p-[10px] border border-gray-200 bg-white rounded-[5px] gap-[10px]" />
+                            <label htmlFor="" className="font-sans font-normal text-base items-center leading-[100%] w-[578px] h-[19px] tracking-tight ">YOUR NAME</label>
+                            <input type="text" placeholder="Enter your name" className="flex w-[578px] h-[46px] p-[10px] border  border-gray-200 bg-white rounded-[5px] gap-[10px]" />
+                            <label htmlFor="" className="font-sans font-normal text-base items-center leading-[100%] w-[578px] h-[19px] tracking-tight">PASSWORD</label>
+                            <input type="password" placeholder="Enter your password" className="flex w-[578px] h-[46px] p-[10px] bg-white  border-gray-200 rounded-[5px] gap-[10px]" />
+                            <label htmlFor="" className="font-sans font-normal text-base items-center leading-[100%] w-[578px] h-[19px] tracking-tight ">CONFIRM YOUR PASSWORD</label>
+                            <input type="password" placeholder="Enter your password" className="flex bg-white  w-[578px] h-[46px] p-[10px]  border-gray-200 rounded-[5px] gap-[10px]" />
+                        </form>
+
+                        <button className="flex justify-center flex-col text-center w-[257px] h-[40px] top-[690px] left-[892px] ml-38 border gap-[10px] bg-[#7848F4] mt-8 pr-[40px] pb-[15px] pl-[40px] text-body-text text-[#FFFFFF] rounded-[5px]">Sign Up</button> <div className="my-4 flex justify-center mr-2 text-center text-gray-500">Or</div>
+
+                        <button className=" border-[1px] border-gray-300 py-2 rounded-[5px] w-[471px] h-[46px] ml-12 flex items-center justify-center gap-2 hover:bg-gray-50 transition bg-white">
+                            <img src={googleIcon} alt="Google" className="w-5 h-5" />
+                            <span>Sign up with Google</span>
+                        </button>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+
+
+
+        </>
+
     );
 }
