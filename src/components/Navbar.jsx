9425e12efdfae7afafbd0416795f05756cc224d0
Naayart <jerusha328@gmail.com>
2025-06-23 08:30:03 -0700

    authentication card

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,30 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import useSWR from "swr";
+import { apiFetcher } from "../api/client";
 
 export default function Navbar() {
+
+const {data} = useSWR('/users/profile', apiFetcher);
+const navigate = useNavigate();
+
+
+    const logout = () => {
+      localStorage.removeItem("ACCESS_TOKEN");
+      navigate("/login")
+    }
+
     return (
         <nav className="flex flex-row justify-between px-12 py-4">
       <h1 className="space-x-1">
        <Link to={'/'} className="text-black font-bold text-2xl">Event</Link>
         <span className="text-[#7848F4] font-bold text-2xl">Hive</span>
       </h1>
+
+      <div>
+        <h1>{data?.data?.name || "Unknown User"}</h1> 
+        <button onClick={logout}>Logout</button>
+      </div>
+      
 
       <div className=" space-x-5 inline-block ">
         <Link to={'login'} className="font-bold ">Login </Link>
